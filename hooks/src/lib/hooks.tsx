8495c81fa6b02f845115b24278ec042406ef68df
Orwell Tendaishe Mushaikwa <mushorwell@hotmail.com>
2025-02-19 23:16:11 +0200

    feat(hooks): create hooks library with initial configuration
    Set up a new hooks library with TypeScript, React, and Vite configuration, including project structure, testing setup, and a basic Hooks component

---
+++ hooks/src/lib/hooks.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledHooks = styled.div`
+  color: pink;
+`;
+export function Hooks() {
+  return (
+    <StyledHooks>
+      <h1>Welcome to Hooks!</h1>
+    </StyledHooks>
+  );
+}
+
+export default Hooks;
