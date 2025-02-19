ca6d4e117949757a045ddb3b1c3250d0007563a8
Orwell Tendaishe Mushaikwa <mushorwell@hotmail.com>
2025-02-19 23:14:54 +0200

    feat(utilities): create utilities library with initial configuration
    Set up a new utilities library with TypeScript, React, and Vite configuration, including basic project structure, testing setup, and initial utility functions

---
+++ utilities/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/renderArrays';
+export * from './lib/showcaseVariants';
+export * from './lib/utilities';
---
+++ utilities/src/lib/utilities.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledUtilities = styled.div`
+  color: pink;
+`;
+export function Utilities() {
+  return (
+    <StyledUtilities>
+      <h1>Welcome to Utilities!</h1>
+    </StyledUtilities>
+  );
+}
+
+export default Utilities;
