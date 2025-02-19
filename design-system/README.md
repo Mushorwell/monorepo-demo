12c1530886fe982cb08013a831eaa3c2e6768a07
Orwell Tendaishe Mushaikwa <mushorwell@hotmail.com>
2025-02-19 03:39:23 +0200

    feat(design-system): setup design-system for component library
    Create the design system which will hold all the components that will be developed and used for the store app

---
+++ design-system/README.md
@@ -0,0 +1,7 @@
+# @monorepo-demo/design-system
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @monorepo-demo/design-system` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ design-system/src/lib/design-system.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledDesignSystem = styled.div`
+  color: pink;
+`;
+export function DesignSystem() {
+  return (
+    <StyledDesignSystem>
+      <h1>Welcome to DesignSystem!</h1>
+    </StyledDesignSystem>
+  );
+}
+
+export default DesignSystem;
